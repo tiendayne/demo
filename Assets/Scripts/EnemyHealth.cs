8ce2d2da678e16bea79c0bb6235f567a794ddac2
tiendayne <148993622+tiendayne@users.noreply.github.com>
2026-03-19 15:30:26 +0700

    demo part 7

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -7,7 +7,7 @@ public class EnemyHealth : Health
 {
     protected override void Die()
     {
-        // Tạo hiệu ứng nổ sử dụng biến explosionPrefab từ lớp cha Health
+        
         if (explosionPrefab != null) {
             var explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
             Destroy(explosion, 1f);
